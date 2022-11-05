a2a7292d32ce84a918e8d02ea8be2ff3f34e8382
khalifaOyabun <ababacarkhalifa13@gmail.com>
2022-11-05 14:46:56 +0100

    first commit 🎉

---
+++ controller/js/router.js
@@ -0,0 +1,27 @@
+// console.log(window.location.pathname);
+const route = (e) => {
+    e = event;
+    e.preventDefault();
+    window.history.pushState({}, "", e.target.href);
+    handleLocation();
+}
+
+const routes = {
+    404: "/views/html/404.html",
+    "/": "/views/html/index.html",
+    "/about": "/views/html/about.html",
+    "/hireme": "/views/html/hireme.html",
+}
+
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes.path || routes[404];
+    const html = await fetch(route).then(data => data.text());
+    const mainPage = document.getElementById("main-page");
+    mainPage.innerHTML = html;
+}
+
+window.onpopstate = handleLocation();
+window.route = route;
+
+handleLocation();
