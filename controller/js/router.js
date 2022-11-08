09d2a9cce877eddb01e0dd983070ef1174eb0a64
khalifaOyabun <ababacarkhalifa13@gmail.com>
2022-11-08 23:21:01 +0100

    spa nav done

--- controller/js/router.js
+++ controller/js/router.js
@@ -7,14 +7,14 @@ const route = (e) => {
 
 const routes = {
     404: "/views/html/404.html",
-    "/": "/views/html/index.html",
+    "/home": "/views/html/banner.html",
     "/about": "/views/html/about.html",
     "/hireme": "/views/html/hireme.html",
 }
 
 const handleLocation = async () => {
     const path = window.location.pathname;
-    const route = routes.path || routes[404];
+    const route = routes[path] || routes[404];
     const html = await fetch(route).then(data => data.text());
     const mainPage = document.getElementById("main-page");
     mainPage.innerHTML = html;
